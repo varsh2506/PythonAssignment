2b7e1f5b130e94821419a20fe849cb1282a5ac2b
Varshini <varshini.srinivasan@gmail.com>
2017-05-10 15:40:04 +0530

    Assignment 2 Submission 2

---
+++ Assignment2(Way2).py
@@ -0,0 +1,36 @@
+#Assignment 2 (Implementation 2)
+def initHistogram(l, number):
+    for i in range(number):
+        l.append([])
+    return l
+
+data = [2,3,4,1,5,9,8,7,2,6]
+bin_size = int(raw_input("Enter the width of each interval: "))
+minimum = min(data)
+maximum = max(data)
+histogram = []
+no_of_bars = ((maximum-minimum)/bin_size)+1
+
+histogram = initHistogram(histogram, no_of_bars)
+
+for value in data:
+    if value%bin_size:
+        barNumber = value/bin_size
+    else:
+        barNumber = (value/bin_size)-1
+    histogram[barNumber].append(value)
+
+l = minimum
+u = l+bin_size-1
+for j in histogram:
+    print str(l)+'-'+str(u),
+    print '-'*len(j)
+    l=u+1
+    u=bin_size+l-1
+
+    
+        
+    
+    
+
+
